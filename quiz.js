69becaa18b8b25b604d680031b08f0a8173d29d8
daniel <dmolnar.seso@gmail.com>
2024-10-08 07:45:23 +0200

    Javascript file with added questions and display questions function

---
+++ quiz.js
@@ -0,0 +1,33 @@
+let score = 0;
+let currentQuestionIndex = 0;
+
+let questions = [
+  {
+    question: "Was ist die Hauptstadt von Ungarn?",
+    answers: ["Paris","London","Berlin", "Budapest"],
+    correctAnswer: 3
+  },
+  {
+    question: "Welche Farbe hat eine Zitrone?",
+    answers: ["Gelb","Blau","Orange", "Grün"],
+    correctAnswer: 0
+  },
+  {
+    question: "Wie viele Beine hat ein Hund?",
+    answers: ["2","6","4", "8"],
+    correctAnswer: 2
+  },
+]
+
+const displayQuestion = () => {
+  let question = questions[currentQuestionIndex];
+  document.getElementById("question").textContent = question.question; 
+  
+  let answersDiv = document.getElementById("answers");
+
+  question.answers.forEach((a,i) => {
+    let answerButton = document.createElement("button");
+    answerButton.textContent = a;
+    answersDiv.appendChild(answerButton);
+  })
+}
