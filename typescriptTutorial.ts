94966ae20aab385c17157cfc7faf2d2c0e5b26ed
daniel <dmolnar.seso@gmail.com>
2024-10-08 10:13:56 +0200

    Typescript Interfaces and Object types with functions

--- typescriptTutorial.ts
+++ typescriptTutorial.ts
@@ -1,13 +1,24 @@
-//let id: number = 5; 
-//let company: string = "Acme Corp";
-//let isPublished:Boolean = true;
-//let ids:number[] = [1,2,3,4,5];
-//let x:any = "pedro";
-//let xArr: any[] = ["pedro", 0 , true];
-//                      parameter types | return type
-const concatValues = (a:string, b:string):string => {
-  return a+b;
+interface UserInterface {
+  id: number;
+  name: string;
+  age: number;
+  extra?:string; //"?"after the key makes this optional and dont get an error when we dont define it 
+  greet(message: string): void; //define the type of Parameter and the return type
 }
+// Interface defines the types of values in an Object
+// You have to define the Objecttype to the Interface in order to work
+const User: UserInterface = {
+  id: 2,
+  name: "Dani",
+  age: 22,
+  greet(massage:string):void {        //types can be defined when you call the function
+    console.log(massage);
+  }
+}
+User.greet("Hello");
 
-console.log(concatValues(10, 5));
-console.log(concatValues("Hello", "World"));
+if(!User.extra) {
+  console.log("No extra defined");
+} else {
+  console.log(User.extra);
+}
