9b37068c3ba10094b947ff6ce65a6a5196615633
daniel <dmolnar.seso@gmail.com>
2024-10-08 09:58:03 +0200

    Typescript types in practice

--- typescriptTutorial.ts
+++ typescriptTutorial.ts
@@ -1,6 +1,13 @@
-let id: number = 5; 
-let company: string = "Acme Corp";
-let isPublished:Boolean = true;
-let ids:number[] = [1,2,3,4,5];
-let x:any = "pedro";
-let xArr: any[] = ["pedro", 0 , true];
+//let id: number = 5; 
+//let company: string = "Acme Corp";
+//let isPublished:Boolean = true;
+//let ids:number[] = [1,2,3,4,5];
+//let x:any = "pedro";
+//let xArr: any[] = ["pedro", 0 , true];
+
+const concatValues = (a,b) => {
+  return a+b;
+}
+
+console.log(concatValues(10, 5));
+console.log("Hello", "World");
