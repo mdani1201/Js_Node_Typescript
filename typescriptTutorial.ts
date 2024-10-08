06029ad7f9b81fa2d49492be7d211ecc2f6b7e6b
daniel <dmolnar.seso@gmail.com>
2024-10-08 09:40:44 +0200

    Typescript types

---
+++ typescriptTutorial.ts
@@ -0,0 +1,6 @@
+let id: number = 5; 
+let company: string = "Acme Corp";
+let isPublished:Boolean = true;
+let ids:number[] = [1,2,3,4,5];
+let x:any = "pedro";
+let xArr: any[] = ["pedro", 0 , true];
