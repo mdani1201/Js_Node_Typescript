d24fa2774f0b2b3049323322a976da25957123df
daniel <dmolnar.seso@gmail.com>
2024-10-08 10:20:39 +0200

    Typescript Union and Custom Types

--- typescriptTutorial.ts
+++ typescriptTutorial.ts
@@ -1,24 +1,14 @@
-interface UserInterface {
-  id: number;
-  name: string;
-  age: number;
-  extra?:string; //"?"after the key makes this optional and dont get an error when we dont define it 
-  greet(message: string): void; //define the type of Parameter and the return type
-}
-// Interface defines the types of values in an Object
-// You have to define the Objecttype to the Interface in order to work
-const User: UserInterface = {
-  id: 2,
-  name: "Dani",
-  age: 22,
-  greet(massage:string):void {        //types can be defined when you call the function
-    console.log(massage);
-  }
-}
-User.greet("Hello");
+//                       union
+const printID = (id: string|number) => {
+  console.log("ID: " + id);
+} 
+// Can be Both types that we defined
+printID("id123");
+printID(12345678);
 
-if(!User.extra) {
-  console.log("No extra defined");
-} else {
-  console.log(User.extra);
-}
+// Custom Types
+type IDField = string|number;
+
+const printID2 = (id: IDField) => {
+  console.log("ID: " + id);
+} 
