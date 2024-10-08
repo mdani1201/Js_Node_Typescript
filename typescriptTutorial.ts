57a2ba3c3ed3cd1cf06de0aa875b304b0ff5f5a5
daniel <dmolnar.seso@gmail.com>
2024-10-08 10:27:45 +0200

    Typescript Intersection

--- typescriptTutorial.ts
+++ typescriptTutorial.ts
@@ -12,3 +12,19 @@ type IDField = string|number;
 const printID2 = (id: IDField) => {
   console.log("ID: " + id);
 } 
+
+interface BusinessPartner {
+  name: string
+  creditScore: number
+}
+
+interface UserIdentity {
+  id:number
+  email: string
+}
+
+type Employee = BusinessPartner & UserIdentity //In intersection you can call both fields from both interfaces
+
+const signContract = (employee: Employee):void => {
+  console.log("Contract signed by" + employee.name + "with email: " + employee.email);
+}
