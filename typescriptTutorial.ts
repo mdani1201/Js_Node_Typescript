678dd2da46e8b8746fee72ee00cb3d6b04222d65
daniel <dmolnar.seso@gmail.com>
2024-10-08 09:59:32 +0200

    Typescript types in practice

--- typescriptTutorial.ts
+++ typescriptTutorial.ts
@@ -4,10 +4,10 @@
 //let ids:number[] = [1,2,3,4,5];
 //let x:any = "pedro";
 //let xArr: any[] = ["pedro", 0 , true];
-
-const concatValues = (a,b) => {
+//                      parameter types | return type
+const concatValues = (a:string, b:string):string => {
   return a+b;
 }
 
 console.log(concatValues(10, 5));
-console.log("Hello", "World");
+console.log(concatValues("Hello", "World"));
